425aad078251513798539e661ac3c29a47196b41
touristtam <tourist.tam@gmail.com>
2014-03-05 14:20:49 +0000

    added axis to equation

--- equation/js/graph.js
+++ equation/js/graph.js
@@ -13,28 +13,33 @@ $( document ).ready( function() {
 });
 
 function drawGrid( vis, width, height, xStep, yStep ) {
-    // the xAxis gives the x coordinates
-    // for vertical lines ( 'y1' = 25 and, 'y2' = height - 25 )
-    var xRange = d3.range( yStep, width, yStep );
+    var xRange =    d3.range( yStep, width, yStep );
+    var xMin =      d3.min( xRange );
+    var xMax =      d3.max( xRange );
+    var xScale =    d3.scale.linear()
+            .domain([ xMin, xMax ])
+            .range([0, width]);
+    var yRange =    d3.range( xStep, height, xStep );
+    var yMin =      d3.min( yRange );
+    var yMax =      d3.max( yRange );
+    var yScale =    d3.scale.linear()
+            .domain([ yMin, yMax ])
+            .range([ 0, height ]);
+    var xCenter = width / 2;
+    var yCenter = height / 2;
 
-    // the yAxis gives the y coordinates
-    // for horizontal lines ( 'x1' = 25 and, 'x2' = width - 25 )
-    var yRange = d3.range( xStep, height, xStep );
+    var gridGroup = vis.append( 'svg:g' );
 
-    var group = vis.append( 'svg:g' );
-
-    // Using the xaxiscoorddata to generate vertical lines.
-    group.selectAll( 'line.vertical' )
+    gridGroup.selectAll( 'line.vertical' )
         .data( xRange )
         .enter().append( 'svg:line' )
         .attr( 'class', 'line' )
-        .attr( 'x1', function( d ) { return d; })
+        .attr( 'x1', function( d ) { /*if( d !== xCenter )*/ return d; })
         .attr( 'y1', yStep )
-        .attr( 'x2', function( d ) { return d; })
+        .attr( 'x2', function( d ) { /*if( d !== xCenter )*/ return d; })
         .attr( 'y2', height - yStep );
 
-    // Using the yaxiscoorddata to generate horizontal lines.
-    group.selectAll( 'line.horizontal')
+    gridGroup.selectAll( 'line.horizontal')
         .data( yRange )
         .enter().append( 'svg:line' )
         .attr( 'class', 'line' )
@@ -42,4 +47,20 @@ function drawGrid( vis, width, height, xStep, yStep ) {
         .attr( 'y1', function( d ) { return d; })
         .attr( 'x2', width - xStep )
         .attr( 'y2', function( d ) { return d; });
+
+    var axisGroup = vis.append( 'svg:g' );
+
+    axisGroup.append( 'svg:line' )
+            .attr( 'class', 'axis' )
+            .attr( 'x1', xScale( 0 ) )
+            .attr( 'y1', height / 2 )
+            .attr( 'x2', xScale( xMax ) )
+            .attr( 'y2', height / 2 );
+
+    axisGroup.append( 'svg:line' )
+            .attr( 'class', 'axis' )
+            .attr( 'x1', width / 2 )
+            .attr( 'y1', yScale( 0 ) )
+            .attr( 'x2', width / 2 )
+            .attr( 'y2', yScale( yMax ) );
 }
