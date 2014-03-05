524ac103ff1b68d34bfbe555a2e661a460eaa96b
touristtam <tourist.tam@gmail.com>
2014-03-05 14:53:59 +0000

    add arrowhead to x and y axis

--- equation/js/graph.js
+++ equation/js/graph.js
@@ -1,25 +1,23 @@
 
 $( document ).ready( function() {
-    var width = 400;
-    var height = 400;
-    var xStep = width / 20;
-    var yStep = height / 20;
-
-    var vis = d3.select( "#graph" ).append( 'svg' )
+    var width = 600;
+    var height = 600;
+    var step = 20;
+    var vis = d3.select( '#graph' ).append( 'svg' )
         .attr( 'width', width )
         .attr( 'height', height );
 
-    drawGrid( vis, width, height, xStep, yStep );
+    drawGrid( vis, width, height, step );
 });
 
-function drawGrid( vis, width, height, xStep, yStep ) {
-    var xRange =    d3.range( yStep, width, yStep );
+function drawGrid( vis, width, height, step ) {
+    var xRange =    d3.range( step, width, step );
     var xMin =      d3.min( xRange );
     var xMax =      d3.max( xRange );
     var xScale =    d3.scale.linear()
             .domain([ xMin, xMax ])
             .range([0, width]);
-    var yRange =    d3.range( xStep, height, xStep );
+    var yRange =    d3.range( step, height, step );
     var yMin =      d3.min( yRange );
     var yMax =      d3.max( yRange );
     var yScale =    d3.scale.linear()
@@ -34,33 +32,48 @@ function drawGrid( vis, width, height, xStep, yStep ) {
         .data( xRange )
         .enter().append( 'svg:line' )
         .attr( 'class', 'line' )
-        .attr( 'x1', function( d ) { /*if( d !== xCenter )*/ return d; })
-        .attr( 'y1', yStep )
-        .attr( 'x2', function( d ) { /*if( d !== xCenter )*/ return d; })
-        .attr( 'y2', height - yStep );
+        .attr( 'x1', function( d ) { return d; })
+        .attr( 'y1', 0 )
+        .attr( 'x2', function( d ) { return d; })
+        .attr( 'y2', height );
 
     gridGroup.selectAll( 'line.horizontal')
         .data( yRange )
         .enter().append( 'svg:line' )
         .attr( 'class', 'line' )
-        .attr( 'x1', xStep )
+        .attr( 'x1', 0 )
         .attr( 'y1', function( d ) { return d; })
-        .attr( 'x2', width - xStep )
+        .attr( 'x2', width )
         .attr( 'y2', function( d ) { return d; });
 
     var axisGroup = vis.append( 'svg:g' );
 
+    axisGroup.append( 'defs' ).append( 'marker' )
+        .attr('id', 'arrowhead')
+        .attr( 'refX', 5 ) /*must be smarter way to calculate shift*/
+        .attr( 'refY', 5 )
+        .attr( 'martkerUnits', 'strokeWidth' )
+        .attr( 'markerWidth', 40 )
+        .attr( 'markerHeight', 40 )
+        .attr( 'orient', 'auto' )
+        .append( 'path' )
+        .attr( 'viewBox', '0 0 10 10' )
+            .attr( 'd', 'M 0 0 L 10 5 L 0 10 z' );
+
     axisGroup.append( 'svg:line' )
             .attr( 'class', 'axis' )
+            .attr( 'marker-end', 'url( #arrowhead )' )
             .attr( 'x1', xScale( 0 ) )
             .attr( 'y1', height / 2 )
-            .attr( 'x2', xScale( xMax ) )
+            .attr( 'x2', xScale( xMax ) - step / 2  )
             .attr( 'y2', height / 2 );
 
     axisGroup.append( 'svg:line' )
             .attr( 'class', 'axis' )
+            .attr( 'marker-end', 'url( #arrowhead )' )
             .attr( 'x1', width / 2 )
             .attr( 'y1', yScale( 0 ) )
             .attr( 'x2', width / 2 )
-            .attr( 'y2', yScale( yMax ) );
+            .attr( 'y2', yScale( yMax ) - step / 2 );
+
 }
