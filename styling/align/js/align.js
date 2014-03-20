ae3e46952428430a88744fa684bcd44f0164c634
touristtam <tourist.tam@gmail.com>
2014-03-20 16:22:00 +0000

    color by level

--- styling/align/js/align.js
+++ styling/align/js/align.js
@@ -1,23 +1,22 @@
 $( function() {
     var elements = $( '.elem' );
-//    var colors = [ '#6AC5AC', '#D64078', '#FDC72F', '#96C02E'  ];
     var colors = [ '#FF0000', '#FF7400', '#009999', '#00CC00', '#D8005F', '#8506A9', '#FFDD00'  ];
     var bColors = [ '#FDC4C4', '#FFE0C6', '#C7FFFF', '#B4E6B4', '#FFC4DE', '#F3C9FF', '#FFF7C6'  ];
     var counter = 0;
 
     $.each( elements, function() {
-        var eColor = colors[counter];
-        var eBackgroundColor = bColors[counter];
         var el = $( this );
         var eType = el.prop( 'tagName' ).toLowerCase();
         var eClasses = el.attr( 'class' ).split( ' ' );
         var activeClass = '';
         var eId = ( el.attr( 'id' )) ? ' id="' + el.attr( 'id' ) + '"' : '';
-
+        counter = getDepth( el );
         $.each( eClasses, function() {
             if( this != 'elem' ) activeClass += ' ' + this;
         });
-        var eClass = activeClass !== ''? ' class="' + $.trim( activeClass ) + '"': '';
+        var eColor = colors[counter];
+        var eBackgroundColor = bColors[counter];
+        var eClass = activeClass !== '' ? ' class="' + $.trim( activeClass ) + '"': '';
         el.css( 'border-color', eColor );
         el.css( 'background-color', eBackgroundColor );
         $( '<span/>', {
@@ -30,10 +29,20 @@ $( function() {
             'css': { 'background-color': eColor },
             'text': '<' + eType + '>'
         }).appendTo( el );
-        ( counter < colors.length - 1 ) ? counter++ : counter = 0;
         el.attr( 'title', '<' + eType + eId + eClass + '>' );
     });
 });
+
+function getDepth( DOMElement )
+{
+    var i = 0;
+    while(( DOMElement = DOMElement.children()).length ) {
+        i++;
+    }
+
+    return i;
+}
+
 
 function hexToRGBA( hex, alpha ) {
     return 'rgba(' + hexToR( hex ) + ',' + hexToG( hex ) + ',' + hexToB( hex ) + ',' + alpha +  ')';
