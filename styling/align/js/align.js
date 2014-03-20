5fe5e0d3b7073e29d7330259eadbb4afbb1792b8
touristtam <tourist.tam@gmail.com>
2014-03-20 15:44:40 +0000

    trying out different way to give a background color to the elem

--- styling/align/js/align.js
+++ styling/align/js/align.js
@@ -1,9 +1,13 @@
 $( function() {
     var elements = $( '.elem' );
-    var colors = [ '#6AC5AC', '#D64078', '#FDC72F', '#96C02E'  ];
+//    var colors = [ '#6AC5AC', '#D64078', '#FDC72F', '#96C02E'  ];
+    var colors = [ '#FF0000', '#FF7400', '#009999', '#00CC00', '#D8005F', '#8506A9', '#FFDD00'  ];
+    var bColors = [ '#FDC4C4', '#FFE0C6', '#C7FFFF', '#B4E6B4', '#FFC4DE', '#F3C9FF', '#FFF7C6'  ];
     var counter = 0;
 
     $.each( elements, function() {
+        var eColor = colors[counter];
+        var eBackgroundColor = bColors[counter];
         var el = $( this );
         var eType = el.prop( 'tagName' ).toLowerCase();
         var eClasses = el.attr( 'class' ).split( ' ' );
@@ -14,8 +18,8 @@ $( function() {
             if( this != 'elem' ) activeClass += ' ' + this;
         });
         var eClass = activeClass !== ''? ' class="' + $.trim( activeClass ) + '"': '';
-        el.css( 'border-color', colors[counter] );
-        var eColor = el.css( 'border-color' );
+        el.css( 'border-color', eColor );
+        el.css( 'background-color', eBackgroundColor );
         $( '<span/>', {
             'class': 'label',
             'css': { 'background-color': eColor },
@@ -26,8 +30,32 @@ $( function() {
             'css': { 'background-color': eColor },
             'text': '<' + eType + '>'
         }).appendTo( el );
-        ( counter < 3 ) ? counter++ : counter = 0;
+        ( counter < colors.length - 1 ) ? counter++ : counter = 0;
         el.attr( 'title', '<' + eType + eId + eClass + '>' );
     });
 });
+
+function hexToRGBA( hex, alpha ) {
+    return 'rgba(' + hexToR( hex ) + ',' + hexToG( hex ) + ',' + hexToB( hex ) + ',' + alpha +  ')';
+}
+
+function hexToRGB( hex ) {
+    return 'rgb(' + hexToR( hex ) + ',' + hexToG( hex ) + ',' + hexToB( hex ) + ')';
+}
+
+function hexToR( h )  {
+    return parseInt( ( cutHex( h ) ).substring( 0, 2 ), 16  );
+}
+
+function hexToG( h ) {
+    return parseInt( ( cutHex( h ) ).substring( 2,4 ), 16 );
+}
+
+function hexToB( h ) {
+    return parseInt( ( cutHex( h )  ).substring( 4,6 ), 16 );
+}
+
+function cutHex( h ) {
+    return ( h.charAt( 0 ) === '#' ) ? h.substring( 1,7 ): h ;
+}
 
