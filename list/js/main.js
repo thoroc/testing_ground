9abc47d7925d4278fb3e49650a748a26485fe92f
touristtam <tourist.tam@gmail.com>
2014-07-28 12:35:47 +0100

    tabbedview

--- list/js/main.js
+++ list/js/main.js
@@ -1,45 +1,14 @@
-//    var menuItems = $( '.fiscal_period_list' ).children( 'li' );
-//    var div = menuItems.first().children( 'div' );
-//    var pos = div.position();
-//    var width = div.outerWidth();
-//    var height = div.outerHeight();
-
-    $( function() {
-        var menuItems = $( '.fiscal_period_list' ).children( 'li' );
-        var div = menuItems.first().children( 'div' );
-        var pos = div.position();
-        var width = div.outerWidth();
-        var height = div.outerHeight();
-
-        alignOptions( menuItems.first(), pos, height );
-//                    console.log( pos, width, height );
-
-        $( '.date_range' ).on( 'click', function( e ) {
-            e.preventDefault();
-            $( this ).parents( 'li' ).siblings( '.active' ).removeClass( 'active' );
-            $( this ).parents( 'li' ).addClass( 'active' );
-            alignOptions( this, pos, height );
+$( function() {
+    $( document ).on( 'click', '.widget-list-link', function( e ) {
+        $( '#fiscal_date_range_start' ).val( $( this ).attr( 'data-start' ) );
+        $( '#fiscal_date_range_end' ).val( $( this ).attr( 'data-end' ) );
+        e.preventDefault();
+        $.each( $( '.widget-list-link.active' ), function() {
+            $( this ).removeClass( 'active' );
         });
+        $( this ).addClass( 'active' );
 
-        $( document ).on( 'click', '.date_range', function( e ) {
-            $( '#fiscal_date_range_start' ).val( $( this ).attr( 'data-start' ) );
-            $( '#fiscal_date_range_end' ).val( $( this ).attr( 'data-end' ) );
-            e.preventDefault();
-            $( this ).siblings( '.current' ).removeClass( 'current' );
-            $( this ).addClass( 'current' );
-
-            return false;
-        });
+        return false;
     });
-
-    function alignOptions( e, p, h ) {
-        var options = $( e ).closest( 'div' ).siblings( 'ul' );
-
-        options.css({
-            position: 'absolute',
-            top: ( p.top + h ) + 'px',
-            left: p.left + 'px'
-        }).show();
-    }
-
+});
 
